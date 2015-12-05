db5e91f0457c4b121487d4d4daa0ce7915e40a14
Olivers De Abreu <oliversd@gmail.com>
2015-12-05 12:58:02 -0300

    Paso 2. Plantillas

--- superpeliculas.js
+++ superpeliculas.js
@@ -1,23 +1,10 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
+  // Este código sólo corre en el cliente
+  Template.body.helpers({
+    peliculas: [
+      { nombre: "Star Wars" },
+      { nombre: "El Padrino" },
+      { nombre: "Forrest Gump" }
+    ]
   });
 }
