2e5770e12f053772b78ff4a38c092d78413af73e
Olivers De Abreu <oliversd@gmail.com>
2015-12-09 21:38:44 -0300

    Insertar nuevas películas a la colección

--- superpeliculas.js
+++ superpeliculas.js
@@ -7,4 +7,30 @@ if (Meteor.isClient) {
       return Peliculas.find({});
     }
   });
+
+  Template.body.events({
+    // Detectamos el evento de submit del formulario
+    'submit .nueva-pelicula': function(event) {
+      // Evitamos el comportamiento por defecto del navegador al enviar
+      // un formulario
+      event.preventDefault();
+
+      // Tomamos los valores de los inputs del formulario
+      var nombre = event.target.nombre.value;
+      var fecha = event.target.fecha.value;
+      var director = event.target.director.value;
+
+      // Insertamos la película en la coleccion
+      Peliculas.insert({
+        nombre: nombre,
+        fecha: fecha,
+        director: director
+      });
+
+      // Limpiamos los valores del formulario
+      event.target.nombre.value = "";
+      event.target.fecha.value = "";
+      event.target.director.value = "";
+    }
+  });
 }
